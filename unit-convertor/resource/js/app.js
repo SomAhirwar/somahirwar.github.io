aa69429b3c61a4fc6906bb5384e23eafa360d046
Som Ahirwar <ahirwar.som@gmail.com>
2020-08-14 14:53:09 +0530

    Making unit-type button working, Make website functional for length unit measures also

--- unit-convertor/resource/js/app.js
+++ unit-convertor/resource/js/app.js
@@ -1,19 +1,29 @@
+var unit = {
+    weight: {
+        microgram: 0.000001,
+        milligram: 0.001,
+        centigram: 0.01,
+        decigram: 0.1,
+        gram: 1,
+        decagram: 10,
+        hectogram: 100,
+        kilogram:1000
+    },
+    length: {
+        nanometer: 0.000000001,
+        micrometer: 0.000001,
+        millimeter: 0.001,
+        centimeter: 0.01,
+        decimeter: 0.1,
+        meter:1,
+        kilometer:1000
+    }
+};
 var unitController = (function() {
-    var unit = {
-        weight: {
-            microgram: 0.000001,
-            milligram: 0.001,
-            centigram: 0.01,
-            decigram: 0.1,
-            gram: 1,
-            decagram: 10,
-            hectogram: 100,
-            kilogram:1000
-        }
-    };
     return {
-        convertUnit: function(u1, u2, value1) {
-            return (unit.weight[u1]/unit.weight[u2]) * value1;
+        convertUnit: function(unitType,u1, u2, value1) {
+            console.log(unitType);
+            return (unit[unitType][u1]/unit[unitType][u2]) * value1;
         }
     };
 })();
@@ -27,7 +37,8 @@ var UIController = (function() {
         value2: 'unit-2-value',
         swapBtn: '.interchange',
         unitRealtionOneToTwo:'.unit-1-to-2',
-        unitRealtionTwotoOne: '.unit-2-to-1'
+        unitRealtionTwotoOne: '.unit-2-to-1',
+        unitType: 'unit-type'
     }
     return {
         getDOMString: function() {
@@ -35,6 +46,7 @@ var UIController = (function() {
         },
         getInput: function() {
             return {
+                uType: document.getElementById(DOMString.unitType).value,
                 u1: document.getElementById(DOMString.u1).value,
                 u2: document.getElementById(DOMString.u2).value,
                 value1: parseFloat(document.getElementById(DOMString.value1).value),
@@ -46,9 +58,25 @@ var UIController = (function() {
             document.getElementById(DOMString.value2).value = value2;
             document.getElementById(DOMString.value2).style.backgroundColor="rgba(200,200,200,0.7)";
         },
-        updateUnitRelation: function(u1,u2, value1, value2, selector) {
-            var relation = value2/value1;
+        updateUnitRelation: function(uType, u1,u2, selector) {
+            var relation = unit[uType][u2]/unit[uType][u1];
+            console.log(unit);
             document.querySelector(selector).textContent = '1 ' + u1 + ' = ' + relation + ' ' + u2;
+        },
+        unitTypeList: function(unitType) {
+            var HTML;
+            switch (unitType) {
+                case 'weight':
+                    HTML = '<option value="kilogram" selected>Kilogram(kg)</option><option value="gram">Gram(g)</option><option value="decagram">Decagram</option><option value="decigram">Decigram</option><option value = "centigram">Centigram</option><option value = "milligram">Milligram(mg)</option><option value = "hectogram">Hectogram</option><option value="microgram">Microgram</option>';
+                    break;
+                case 'length':
+                    HTML = '<option value="kilometer" selected>Kilometer(km)</option><option value="meter">Meter(m)</option><option value="decimeter">Decimeter(dm)</option><option value = "centimeter">Centimeter(cm)</option><option value = "millimeter">Millimeter(mm)</option><option value = "micrometer">Micrometer</option><option value="nanometer">Nanometer</option>';
+                    break;
+            };
+            document.getElementById(DOMString.u1).innerHTML = '';
+            document.getElementById(DOMString.u2).innerHTML = '';
+            document.getElementById(DOMString.u1).insertAdjacentHTML('afterbegin',HTML);
+            document.getElementById(DOMString.u2).insertAdjacentHTML('afterbegin',HTML);
         }
     };
 })();
@@ -58,19 +86,19 @@ var controller = (function(unitCtrl, UICtrl) {
 
     var convert = function() {
         var input, value2;
-        //1. Take unit1, unit 2 type and also unit1 value;
+        //1. Take unit1, unit 2, unit-type type and also unit1 value;
         input = UICtrl.getInput();
         //console.log(input);
         //2. Convert u1 to u2
         if(!isNaN(input.value1) && input.value1 != 0)
         {
-            value2 = unitCtrl.convertUnit(input.u1, input.u2, input.value1);
+            value2 = unitCtrl.convertUnit(input.uType,input.u1, input.u2, input.value1);
             //3. Update UI
             UICtrl.updateValue2(value2);
     
             //4. Updating unit relation
-            UICtrl.updateUnitRelation(input.u1, input.u2, input.value1, value2, DOM.unitRealtionOneToTwo);
-            UICtrl.updateUnitRelation(input.u2, input.u1, value2, input.value1, DOM.unitRealtionTwotoOne);
+            UICtrl.updateUnitRelation(input.uType, input.u1, input.u2, DOM.unitRealtionOneToTwo);
+            UICtrl.updateUnitRelation(input.uType, input.u2, input.u1, DOM.unitRealtionTwotoOne);
 
         }
     }
@@ -95,12 +123,11 @@ var controller = (function(unitCtrl, UICtrl) {
         }
 
             //4. Updating unit relation
-            /*UICtrl.updateUnitRelation(u1, u2, value1, value2, DOM.unitRealtionOneToTwo);
-            UICtrl.updateUnitRelation(u2, u1, value2, value1, DOM.unitRealtionTwotoOne);*/
             temp = document.querySelector(DOM.unitRealtionOneToTwo).textContent;
             document.querySelector(DOM.unitRealtionOneToTwo).textContent = document.querySelector(DOM.unitRealtionTwotoOne).textContent;
             document.querySelector(DOM.unitRealtionTwotoOne).textContent = temp;        
     }
+
 
     var setupEventListener = function() {
         document.querySelector(DOM.convertBtn).addEventListener('click', convert);
@@ -110,11 +137,14 @@ var controller = (function(unitCtrl, UICtrl) {
             }
         });
         document.querySelector(DOM.swapBtn).addEventListener('click', swap);
+        document.getElementById(DOM.unitType).addEventListener('click', (e) => {
+            if(e.target.nodeName === 'OPTION') {
+                UICtrl.unitTypeList(e.target.value);
+            }
+        });
     };
     return {
         init: function() {
-            /*console.log("it's InIt");
-            console.log(DOM);*/
             setupEventListener();
         }
     }
