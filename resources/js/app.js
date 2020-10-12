c1af82a7217735eb9e04f4b0d3f2a6d856558e5f
Som Ahirwar <ahirwar.som@gmail.com>
2020-10-12 21:02:42 +0530

    Creating Footer Fixing Animation

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,9 +4,25 @@ projects.forEach((el,i) => {
     console.log(el);
 });
 
+function debounce(func, wait = 20, immediate = true) {
+    var timeout;
+    return function() {
+      var context = this, args = arguments;
+      var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  };
+
 function slideIn(el) {
+    console.log(el);
     projects.forEach(e => {
-        const current = el.pageY + window.innerHeight;
+        const current = window.scrollY + window.innerHeight;
         console.log(current, e.offsetTop + 1/2 * e.offsetHeight);
         if(current > e.offsetTop + 1/2 * e.offsetHeight)
             e.style.transform = `translateX(0)`;
@@ -15,4 +31,4 @@ function slideIn(el) {
     });
 }
 
-window.addEventListener('scroll', slideIn);
+window.addEventListener('scroll', debounce(slideIn));
