5301796dc987eac959ac8163f8e216ae913191cd
Som Ahirwar <ahirwar.som@gmail.com>
2020-12-13 22:32:29 +0530

    Making Homepage responsive. Adding Animation 	modified:   index.html 	new file:   resources/css/query.css 	modified:   resources/css/style.css 	modified:   resources/js/app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,34 +1,38 @@
-const projects = document.querySelectorAll('.project');
-projects.forEach((el,i) => {
-    el.style.marginLeft = `${(i)*10 + 10}vw`;
-    console.log(el);
-});
+const projects = document.querySelectorAll(".project");
+const burger = document.querySelector(".burger");
 
-function debounce(func, wait = 20, immediate = true) {
-    var timeout;
-    return function() {
-      var context = this, args = arguments;
-      var later = function() {
-        timeout = null;
-        if (!immediate) func.apply(context, args);
-      };
-      var callNow = immediate && !timeout;
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
-      if (callNow) func.apply(context, args);
+function debounce(func, wait = 10, immediate = true) {
+  var timeout;
+  return function () {
+    var context = this,
+      args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
     };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
   };
-
-function slideIn(el) {
-    console.log(el);
-    projects.forEach(e => {
-        const current = window.scrollY + window.innerHeight;
-        console.log(current, e.offsetTop + 1/2 * e.offsetHeight);
-        if(current > e.offsetTop + 1/2 * e.offsetHeight)
-            e.style.transform = `translateX(0)`;
-        else
-        e.style.transform = ``;
-    });
 }
 
-window.addEventListener('scroll', debounce(slideIn));
+function slideIn(el) {
+  projects.forEach((e) => {
+    const current = window.scrollY + window.innerHeight;
+    if (current > e.offsetTop + (1 / 5) * e.offsetHeight)
+      e.style.transform = `translateX(0)`;
+    else e.style.transform = ``;
+  });
+}
+
+function displayDropbox() {
+  const dropbox = document.querySelector("nav ul");
+
+  if (dropbox.style.transform === `translateX(100%)`)
+    dropbox.style.transform = `translateX(0)`;
+  else dropbox.style.transform = `translateX(100%)`;
+}
+
+window.addEventListener("scroll", debounce(slideIn));
+burger.addEventListener("click", displayDropbox);
